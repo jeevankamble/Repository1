3350c798488516873404d49d5ff4633e046d4039
Your Name <you@example.com>
2025-02-07 13:03:26 +0530

    added config file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+MODEL_FILE_PATH = os.path.join(os.getcwd(), r'artifacts\adaboost_clf_model_file.pkl')
+
+JSON_FILE_PATH = os.path.join(os.getcwd(), r'artifacts\project_data.json')
+
+CSV_FILE_PATH = os.path.join(os.getcwd(), r'CSV_file\healthcare-dataset-stroke-data.csv')
+
+ARTIFATCS_FOLDER_PATH = 'artifacts'
+
+PORT_NUMBER = 5004
